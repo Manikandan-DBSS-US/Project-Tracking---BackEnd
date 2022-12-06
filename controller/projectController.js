3e1503ca25a276dc3af7f9d486c8d4e8103b68df
Manikandan-DBSS-US <Manikandan@dynamicbiz.us>
2022-12-06 20:07:52 +0530

    CRUD in Project

--- controller/projectController.js
+++ controller/projectController.js
@@ -7,6 +7,17 @@ import {
 import { startSession } from "mongoose";
 import { StatusCodes } from "http-status-codes";
 
+const getAllProjects = async (req, res) => {
+  const projects = await Project.find({});
+  res.status(StatusCodes.OK).json({ projects });
+};
+
+const getOneProject = async (req, res) => {
+  const { id: projectId } = req.params;
+  const project = await Project.findById({ _id: projectId });
+  res.status(StatusCodes.OK).json({ project });
+};
+
 const CreateProject = async (req, res) => {
   const {
     projectName,
@@ -27,8 +38,59 @@ const CreateProject = async (req, res) => {
   ) {
     throw new BadRequestError("Please Provide All Values");
   }
-  const data = await Project.create(req.body);
-  res.status(StatusCodes.CREATED).json({ data });
+  const project = await Project.create(req.body);
+  res.status(StatusCodes.CREATED).json({ project });
 };
 
-export { CreateProject };
+const UpdateProject = async (req, res) => {
+  const { id: projectId } = req.params;
+  const {
+    projectName,
+    projectDescription,
+    clientName,
+    projectStartDate,
+    projectEndDate,
+    technologiesUsed,
+  } = req.body;
+  if (
+    !projectName ||
+    !projectDescription ||
+    !clientName ||
+    !projectStartDate ||
+    !projectEndDate ||
+    !technologiesUsed
+  ) {
+    throw new BadRequestError("Please Provide All Values");
+  }
+  const project = await Project.findOne({ _id: projectId });
+  if (!project) {
+    throw new NotFoundError(`not available this id : ${projectId}`);
+  }
+  const updatedProject = await Project.findByIdAndUpdate(
+    { _id: projectId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(StatusCodes.OK).json({ updatedProject });
+};
+
+const deleteProject = async (req, res) => {
+  const { id: projectId } = req.params;
+  const project = await Project.findOne({ _id: projectId });
+  if (!project) {
+    throw new NotFoundError(`No job with id :${projectId}`);
+  }
+  await project.remove();
+  res.status(StatusCodes.OK).json({ msg: "Successfully! deleted" });
+};
+
+export {
+  CreateProject,
+  getAllProjects,
+  getOneProject,
+  UpdateProject,
+  deleteProject,
+};
--- router/projectRouter.js
+++ router/projectRouter.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { CreateProject } from "../controller/projectController.js";
+import {
+  CreateProject,
+  deleteProject,
+  getAllProjects,
+  getOneProject,
+  UpdateProject,
+} from "../controller/projectController.js";
 const router = express.Router();
 
 router.route("/create").post(CreateProject);
+router.route("/getAllProjects").get(getAllProjects);
+router.route("/getOneProject/:id").get(getOneProject);
+router.route("/update/:id").put(UpdateProject);
+router.route("/delete/:id").delete(deleteProject);
 
 export default router;
