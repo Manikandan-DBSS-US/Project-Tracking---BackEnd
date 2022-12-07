22517bea767648a11c7fc6853743f2783fad80cc
Maha DynamicBiz <maharaja@dynamicbiz.us>
2022-12-07 11:42:47 +0530

    maha-user List added

--- controller/authController.js
+++ controller/authController.js
@@ -6,7 +6,11 @@ import {
 } from "../error/index.js";
 import { StatusCodes } from "http-status-codes";
 import { hashPassword, hashValidator } from "../utils/hashPassword.js";
-import { accessToken, refreshToken, refreshTokenValidator } from "../utils/token.js";
+import {
+  accessToken,
+  refreshToken,
+  refreshTokenValidator,
+} from "../utils/token.js";
 
 const register = async (req, res) => {
   const {
@@ -22,6 +26,18 @@ const register = async (req, res) => {
     role,
     isActive,
   } = req.body;
+  if (
+    !userId ||
+    !userName ||
+    !firstName ||
+    !lastName ||
+    !password ||
+    !email ||
+    !dateOfBirth ||
+    !gender ||
+    !phoneNumber
+  )
+    throw new BadRequestError("Please provide all values");
   const userExist = await User.findOne({ email });
   if (userExist) throw new BadRequestError("Email already in use");
   if (password.length < 6)
@@ -41,7 +57,7 @@ const register = async (req, res) => {
     role,
     isActive,
   });
-  res.status(StatusCodes.CREATED).json({ msg:"Register Success!" });
+  res.status(StatusCodes.CREATED).json({ msg: "Register Success!" });
 };
 
 const login = async (req, res) => {
@@ -51,18 +67,18 @@ const login = async (req, res) => {
   const userExist = await User.findOne({ userName });
   if (!userExist) throw new UnAuthenticatedError("username does not exist");
   const isPasswordCorrect = await hashValidator(password, userExist.password);
-  if (!isPasswordCorrect) throw new UnAuthenticatedError("wrong password");
-
+  if (!isPasswordCorrect)
+    throw new UnAuthenticatedError("Enter correct password");
   const refresh_token = await refreshToken({ id: userExist._id });
-  console.log({ refresh_token ,userExist});
+  console.log({ refresh_token, userExist });
 
-  res.cookie("refreshtoken",refresh_token, {
-    httpOnly:true,
-    path:"/api/v1/user/refresh_token",
-    maxAge: 7 * 24 * 60 * 60 * 1000
-}) 
+  res.cookie("refreshtoken", refresh_token, {
+    httpOnly: true,
+    path: "/api/v1/user/refresh_token",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
-  res.status(StatusCodes.CREATED).json("login successfull");
+  res.status(StatusCodes.CREATED).json({ userExist });
 };
 
 const getAccessToken = async (req, res) => {
@@ -70,32 +86,90 @@ const getAccessToken = async (req, res) => {
   if (!rf_token) throw new UnAuthenticatedError("Please login now!");
   const userValidation = await refreshTokenValidator(rf_token);
   if (!userValidation) throw new UnAuthenticatedError("Please login now!");
-  const access_token = await accessToken({id:userValidation.id})
-  res.status(StatusCodes.CREATED).json({access_token})
+  const access_token = await accessToken({ id: userValidation.id });
+  res.status(StatusCodes.CREATED).json({ access_token });
 };
 
-
-const logout = async(req,res) => {
-  res.clearCookie("refreshtoken",{
-    path:"/api/v1/user/refresh_token"
+const logout = async (req, res) => {
+  res.clearCookie("refreshtoken", {
+    path: "/api/v1/user/refresh_token",
   });
-  res.status(StatusCodes.OK).json({msg:'Logged out!'})
+  res.status(StatusCodes.OK).json({ msg: "Logged out!" });
 };
 
-const getAlluser = async(req,res) => {
+const getAlluser = async (req, res) => {
+  const users = await User.find({}).select("-password");
+  res.status(StatusCodes.OK).json({ users });
+};
+
+const getSpecificUser = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById({ _id: id });
+  res.status(StatusCodes.OK).json({ user });
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const {
+    userId,
+    firstName,
+    lastName,
+    email,
+    dateOfBirth,
+    gender,
+    phoneNumber,
+    role,
+    isActive,
+  } = req.body;
+  const user = await User.findOne({ _id: id });
+  if (!user) throw new NotFoundError(`No User with id :${id}`);
+  if (
+    !userId ||
+    !firstName ||
+    !lastName ||
+    !email ||
+    !dateOfBirth ||
+    !gender ||
+    !phoneNumber
+  )
+    throw new BadRequestError("Please provide all values");
+  console.log(req.body);
+  const updatedUser = await User.findOneAndUpdate(
+    { _id: id },
+    {
+      userId,
+      firstName,
+      lastName,
+      email,
+      dateOfBirth,
+      gender,
+      phoneNumber,
+      role,
+      isActive,
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+  res.status(StatusCodes.OK).json({ updatedUser });
+};
+
+const deleteUser = async (req, res) => {
+  const {id} = req.params;
+  const user = await User.findOne({ _id: id });
+  if (!user) throw new NotFoundError(`No User with id :${id}`);
+  const deletedUser = await User.findOneAndDelete({_id:id})
+  res.status(StatusCodes.OK).json({ deleteUser });
 
 };
 
-const getSpecificUser = async(req,res) => {
-  
+export {
+  register,
+  login,
+  getAccessToken,
+  logout,
+  getAlluser,
+  getSpecificUser,
+  updateUser,
+  deleteUser
 };
-
-const updateUser = async(req,res) => {
-
-};
-
-const deleteUser = async(req,res) => {
-  
-}
-
-export { register, login, getAccessToken,logout };
--- router/authRouter.js
+++ router/authRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getAccessToken, login, logout, register } from '../controller/authController.js';
+import { deleteUser, getAccessToken, getAlluser, getSpecificUser, login, logout, register, updateUser } from '../controller/authController.js';
 
 const router  = express.Router();
 
 
+router.route("/").get(getAlluser)
+router.route("/:id").get(getSpecificUser).put(updateUser).delete(deleteUser)
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/refresh_token').post(getAccessToken);
